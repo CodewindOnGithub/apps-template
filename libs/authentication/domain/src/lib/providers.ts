2e46bb4e53bce5b1b4bacb2ee35eade557ef42ad
Jonas Stein <jonasstein@Air-von-Nubahar.fritz.box>
2022-12-01 21:48:31 +0100

    chore(app): fix eslint rules

--- libs/authentication/domain/src/lib/providers.ts
+++ libs/authentication/domain/src/lib/providers.ts
@@ -3,9 +3,7 @@ import { provideState } from '@ngrx/store';
 import { UserEffects } from './+state/user/user.effects';
 import { reducer, USER_FEATURE_KEY } from './+state/user/user.reducer';
 
-export const provideAuthenticationDomain = () => {
-   return [
-    provideState(USER_FEATURE_KEY, reducer),
-    provideEffects([UserEffects]),
-  ];
-};
+export const provideAuthenticationDomain = () => [
+  provideState(USER_FEATURE_KEY, reducer),
+  provideEffects([UserEffects]),
+];
